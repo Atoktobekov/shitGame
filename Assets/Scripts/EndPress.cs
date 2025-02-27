e5c80687ad93acab47611e5ec6410c1660924eb2
Adi <atoktobekov1@gmail.com>
2025-02-27 16:12:29 +0600

    add Start and End prefabs, make ChasingSaw prefab Start doesn't have any animation End has "Pressed" animation which is playing after Player has touched it's collider

---
+++ Assets/Scripts/EndPress.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+
+public class EndPress : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(PlayAnimation());
+        }
+    }
+
+    private IEnumerator PlayAnimation()
+    {
+        anim.SetTrigger("Press"); // Запускаем анимацию
+        yield return new WaitForSeconds(2.3f); // Ждем 3 секунды
+        anim.SetTrigger("Idle"); // Возвращаем анимацию в Idle
+    }
+}
