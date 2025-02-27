64f2c7b6168b18aa89fe96640f4a83ac37904e5e
Adi <atoktobekov1@gmail.com>
2025-02-27 15:49:46 +0600

    add Saw and ChasingSaw, add animations for them

---
+++ Assets/Scripts/ChasingSaw.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public class ChasingSaw : MonoBehaviour
+{
+    public float speed = 3f; // Скорость движения пилы
+    public float detectionRange = 5f; // Радиус обнаружения игрока
+    private Transform player;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, 200 * Time.deltaTime); // 200 - скорость вращения
+
+        if (player == null) return;
+
+        float distance = Vector2.Distance(transform.position, player.position);
+        if (distance <= detectionRange)
+        {
+            // Двигаемся к игроку
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.takeLive();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SawDamage.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SawDamage : MonoBehaviour
+{
+    public float knockbackForce = 9.22f;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+           PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+           if (playerHealth != null)
+           {
+               playerHealth.takeLive(); // Игрок получает урон
+               Vector2 knockbackDirection = (other.transform.position - transform.position).normalized; // Направление отталкивания
+               other.gameObject.GetComponent<PlayerController>().getDamageFromSpikes(knockbackDirection , knockbackForce);
+           }
+        }
+    }
+    
+    
+}
