3e38d7af964b33d99c73a8f52d6749ce7809e4ac
Adi <atoktobekov1@gmail.com>
2025-04-17 14:48:51 +0600

    add AboutMenu, add new options in OptionsMenu
    -AboutMenu: has basic info about game and used music, also there are links for feedback
    
    -OptionsMenu: add new Fullscreen toggle and Mute toggle

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,7 +6,10 @@ public class OptionsMenu : MonoBehaviour
 {
     public Slider musicSlider;
     public Slider sfxSlider;
-
+    public TMP_Text fullscreenText;
+    public TMP_Text muteText;
+    private bool isMuted = false;
+    private float savedVolume = 1f;
     private void Start()
     {
         musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.35f);
@@ -13,6 +17,7 @@ public class OptionsMenu : MonoBehaviour
 
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        UpdateFullscreenText();
     }
 
     private void SetMusicVolume(float volume)
@@ -23,5 +28,33 @@ public class OptionsMenu : MonoBehaviour
     private void SetSFXVolume(float volume)
     {
         AudioManager.instance.SetSFXVolume(volume);
+    }
+    public void ToggleFullscreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+        UpdateFullscreenText();
+    }
+
+    void UpdateFullscreenText()
+    {
+        Debug.Log("Fullscreen mode changed");
+        fullscreenText.text = (Screen.fullScreen ? "ON" : "OFF");
+    }
+    
+    
+    public void ToggleMute()
+    {
+        if (isMuted)
+        {
+            AudioListener.volume = savedVolume;
+        }
+        else
+        {
+            savedVolume = AudioListener.volume;
+            AudioListener.volume = 0f;
+        }
+
+        isMuted = !isMuted;
+        muteText.text = (isMuted ? "ON" : "OFF");
     }
 }
